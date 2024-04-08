92b46e517e767ad6f52484ee20cc0cc696ace0a7
RobertoPalici <palici.roberto@gmail.com>
2024-04-08 17:57:54 +0300

    .

--- ProjectStructure/demo/src/main/java/com/restservice/shoppingListAndInventory/inventory/Product.java
+++ ProjectStructure/demo/src/main/java/com/restservice/shoppingListAndInventory/inventory/Product.java
@@ -1,39 +1,18 @@
 package com.restservice.shoppingListAndInventory.inventory;
-
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDate;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
 public class Product {
     String name;
     LocalDate expiryDate;
     float quantity;
     int averageConsumption;
     public void computeAverageConsumption(){
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public LocalDate getExpiryDate() {
-        return expiryDate;
-    }
-
-    public void setExpiryDate(LocalDate expiryDate) {
-        this.expiryDate = expiryDate;
-    }
-
-    public float getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(float quantity) {
-        this.quantity = quantity;
     }
 }
