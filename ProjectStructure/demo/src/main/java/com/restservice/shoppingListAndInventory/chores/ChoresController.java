ba32dbbf803e9b6f8fdeb5e35930f8ef35c88896
pereteD <galbeazad@gmail.com>
2024-05-23 00:04:33 +0300

    .

--- ProjectStructure/demo/src/main/java/com/restservice/shoppingListAndInventory/chores/ChoresController.java
+++ ProjectStructure/demo/src/main/java/com/restservice/shoppingListAndInventory/chores/ChoresController.java
@@ -161,6 +161,6 @@ public class ChoresController {
     }
     @GetMapping("getHistory")
     public ChoresHistoryList getHistory(){
-        return household.getChoresHistoryList();
+        return household.choresHistoryList;
     }
 }
