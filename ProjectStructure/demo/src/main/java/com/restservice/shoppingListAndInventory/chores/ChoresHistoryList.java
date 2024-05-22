7e31f90d6d399bb2ba0040c6e7925f1e40c3868c
pereteD <galbeazad@gmail.com>
2024-05-23 00:19:57 +0300

    .

--- ProjectStructure/demo/src/main/java/com/restservice/shoppingListAndInventory/chores/ChoresHistoryList.java
+++ ProjectStructure/demo/src/main/java/com/restservice/shoppingListAndInventory/chores/ChoresHistoryList.java
@@ -34,7 +34,7 @@ public class ChoresHistoryList {
     @JoinColumn(name = "household_id", referencedColumnName = "id")
     private Household household;
 
-    @OneToMany(mappedBy = "list")
+    @OneToMany(mappedBy = "list", fetch = FetchType.EAGER)
     @Cascade(org.hibernate.annotations.CascadeType.ALL)
     List<ChoreHistoryItem> choresList = new ArrayList<>();
 
