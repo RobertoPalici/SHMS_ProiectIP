26341080bd8f4ae15249388bc410e68729348bf3
pereteD <galbeazad@gmail.com>
2024-05-27 13:54:54 +0300

    .

--- ProjectStructure/demo/src/main/java/com/restservice/shoppingListAndInventory/notifications/NotificationsList.java
+++ ProjectStructure/demo/src/main/java/com/restservice/shoppingListAndInventory/notifications/NotificationsList.java
@@ -38,7 +38,7 @@ public class NotificationsList {
     private Household household;
 
     @LazyCollection(LazyCollectionOption.FALSE)
-    @OneToMany(mappedBy = "list", cascade = CascadeType.PERSIST, orphanRemoval = true)
+    @OneToMany(mappedBy = "list", cascade = CascadeType.ALL, orphanRemoval = true)
     List<Notification> notificationList = new ArrayList<>();
 
 
@@ -48,7 +48,6 @@ public class NotificationsList {
     public void clearNotifications(NotificationRepository notificationRepository){
         while(!notificationList.isEmpty()){
             Notification notification=notificationList.get(0);
-            notificationRepository.notificationItemRepository.delete(notification);
             notificationList.remove(0);
         }
     }
