8fddeb9340a62df17c38a76a177bf806d420108c
pereteD <galbeazad@gmail.com>
2024-05-28 17:23:21 +0300

    Update Product.java

--- ProjectStructure/demo/src/main/java/com/restservice/shoppingListAndInventory/inventory/Product.java
+++ ProjectStructure/demo/src/main/java/com/restservice/shoppingListAndInventory/inventory/Product.java
@@ -14,7 +14,7 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @Entity
 @Table(name = "product_temporary")
-@JsonIgnoreProperties(value = {"id", "isEatable"})
+@JsonIgnoreProperties(value = {"isEatable"})
 public class Product {
     @Id
     @Column(name = "id")
